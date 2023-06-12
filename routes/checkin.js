c238d527ea36a8c3080bf69337ebe823b26da176
Zhan Yang <zyang@conviva.com>
2023-06-12 17:23:47 +0800

    feat: fix replacement issue.

--- routes/checkin.js
+++ routes/checkin.js
@@ -6,8 +6,8 @@ const LOG_TYPE = {
   // 日常打卡
   NORMAL: 0,
   // 补签
-  REPLACEMENT: 1
-}
+  REPLACEMENT: 1,
+};
 
 /* GET tasks list. */
 router.get('/list', async function (req, res, next) {
@@ -18,20 +18,12 @@ router.get('/list', async function (req, res, next) {
 /* POST create new checkin log. */
 router.post('/create', async function (req, res, next) {
   const reqBody = req.body;
-  const keys = ['user_id', 'task_id', 'date'];
-  const isReplacement = reqBody.type !== undefined
-  if (!isReplacement) {
-    // 日常打卡
-    await query(
-        `INSERT INTO checkin_logs (${keys}) values (${reqBody['user_id']}, ${reqBody['task_id']}, '${reqBody['date']}')`,
-    );
-  } else {
-    // 补签打卡
-    keys.push('type')
-    await query(
-        `INSERT INTO checkin_logs (${keys}) values (${reqBody['user_id']}, ${reqBody['task_id']}, '${reqBody['date']}', ${LOG_TYPE.REPLACEMENT})`,
-    );
-  }
+  const keys = ['user_id', 'task_id', 'date', 'type'];
+  const isReplacement = reqBody.type !== undefined && reqBody.type === LOG_TYPE.REPLACEMENT;
+  await query(
+    `INSERT INTO checkin_logs (${keys}) values (${reqBody['user_id']}, ${reqBody['task_id']},
+ '${reqBody['date']}', ${isReplacement ? LOG_TYPE.REPLACEMENT : LOG_TYPE.NORMAL})`,
+  );
   res.jsonResponse({});
 });
 
