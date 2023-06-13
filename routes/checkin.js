78b1be4f5b05d7977ee3ccb7b54b56eaab672b27
Zhan Yang <zyang@conviva.com>
2023-06-13 15:05:34 +0800

    feat: format code.

--- routes/checkin.js
+++ routes/checkin.js
@@ -11,8 +11,8 @@ const LOG_TYPE = {
 
 /* GET tasks list. */
 router.get('/list', async function (req, res, next) {
-  const user_id = req.query['user_id']
-  let results = []
+  const user_id = req.query['user_id'];
+  let results = [];
   if (user_id) {
     results = await query(`SELECT * FROM checkin_logs WHERE user_id = ${user_id}`);
   } else {
